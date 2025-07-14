48a13d9bb193dd36fe961676246c97e4c059579f
Caspar Lehmkühler <casparlehmkuhler@mac.speedport.ip>
2025-07-15 00:20:19 +0200

    bug fixes

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,25 +1,15 @@
-import { NextResponse } from 'next/server'
-
-export function middleware(request) {
-  const basicAuth = request.headers.get('authorization')
+export default function handler(req, res) {
+  const basicAuth = req.headers.authorization
   
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
+    const [user, pwd] = Buffer.from(authValue, 'base64').toString().split(':')
     
     if (user === 'lyceum' && pwd === 'supersecretpassword') {
-      return NextResponse.next()
+      return res.status(200).end()
     }
   }
   
-  return new Response('Authentication required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Restricted"'
-    }
-  })
-}
-
-export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  res.setHeader('WWW-Authenticate', 'Basic realm="Restricted"')
+  res.status(401).send('Authentication required')
 }
